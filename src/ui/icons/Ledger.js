681843dcfda3829797533f0965d151de3eeddd5a
gagarin55 <gagarin55@protonmail.com>
2017-11-06 16:30:30 +0300

    Ledger icon

---
+++ src/ui/icons/Ledger.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import SvgIcon from 'material-ui/SvgIcon';
+
+const Ledger = (props) => (
+    <SvgIcon {...props}>
+        <path d="M5.158 0h4.42A4.424 4.424 0 0 1 14 4.421v4.421H5.158V0zM0 1.842C0 .825.818 0 1.842 0h1.842v3.684H0V1.842zm0 10.316v-1.842h3.684V14H1.842A1.838 1.838 0 0 1 0 12.158zm14 0A1.838 1.838 0 0 1 12.158 14h-1.842v-3.684H14v1.842zm-14-7h3.684v3.684H0V5.158zm5.158 5.158h3.684V14H5.158v-3.684z" />
+    </SvgIcon>
+);
+
+export default Ledger;
