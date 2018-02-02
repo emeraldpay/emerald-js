0f9d3d0b8149b27f93e11e2f5a9b513c741a9721
gagarin55 <gagarin55@protonmail.com>
2018-02-02 17:22:38 +0300

    index.d.ts updated

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,7 +4,13 @@ export declare class EthApi {
     getBalance(address: string, blockNumber: number | string = 'latest'): Promise<BigNumber>
 }
 
-export class EthRpc {
+export declare class JsonRpc {
+
+}
+
+export declare class EthRpc {
 
     public eth: EthApi;
+
+    constructor(jsonRpc: JsonRpc);
 }
