56e89b28983b2a15feb6fef6a867702334cf29ed
gagarin55 <gagarin55@protonmail.com>
2018-02-02 17:27:44 +0300

    index.d.ts updated

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,20 @@
 import BigNumber from "bignumber.js";
 
-export declare class EthApi {
-    getBalance(address: string, blockNumber: number | string = 'latest'): Promise<BigNumber>
+
+export declare interface Transport {
+
+}
+
+export declare class HttpTransport implements Transport {
+    constructor(url: string);
 }
 
 export declare class JsonRpc {
+    constructor(transport: Transport)
+}
 
+export declare class EthApi {
+    getBalance(address: string, blockNumber: number | string = 'latest'): Promise<BigNumber>
 }
 
 export declare class EthRpc {
