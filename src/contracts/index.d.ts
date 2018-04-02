63677357e436a47194b5b119d6a6c0666de3da45
gagarin55 <gagarin55@protonmail.com>
2018-04-02 18:36:13 +0300

    dataToParams typings

--- src/contracts/index.d.ts
+++ src/contracts/index.d.ts
@@ -6,4 +6,11 @@ interface InputValues {
     [name: string]: string;
 }
 
+interface OutputValue {
+    type: string;
+    name: string;
+    value: string | number;
+}
+
 export declare function functionToData(func: AbiMethod, inputs: InputValues): string;
+export declare function dataToParams(func: AbiMethod, data: string): Arrya<OutputValue>;
