2a78e57e073b72e3afdf6b0789848f0588ccca86
Zachary Belford <belfordz66@gmail.com>
2018-11-19 08:10:29 -0800

    tz-uri

--- src/transactionUri.js
+++ src/transactionUri.js
@@ -1,27 +1,48 @@
+import { rlp } from 'ethereumjs-util';
 import * as qs from 'qs';
+import { BigNumber } from 'bignumber.js';
 
-export default class TransactionUri {
-  constructor(abi, transaction) {
-    this.abi = abi;
+type TAddress = string;
+
+interface ITransaction {
+  from?: TAddress;
+  to?: TAddress;
+  value?: BigNumber | string | number;
+  data?: string;
+  gas?: BigNumber | string | number;
+  gasPrice?: BigNumber | string | number;
+}
+
+export default class Transaction {
+  constructor(
+    public from?: TAddress,
+    public to?: TAddress,
+    public value?: BigNumber | string | number,
+    public data?: string,
+    public gas?: BigNumber | string | number,
+    public gasPrice?: BigNumber | string | number
+  ) {
+    this.from = from;
+    this.to = to;
+    this.value = value;
+    this.data = data;
+    this.gas = gas;
+    this.gasPrice = gasPrice;
+  }
+}
+
+
+class TransactionUri {
+  constructor(transaction, abi) {
     this.transaction = transaction;
+    this.abi = abi;
   }
 
   toString() {
-    let additionalTxParams = {};
-    if (this.transaction.method) {
-      additionalTxParams = {
-        mode: 'contract_function',
-        functionSignature: this.props.abi.find((item) => item.name === this.transaction.method),
-        argsDefaults: this.transaction.params,
-        data: this.transaction.data,
-      };
-    }
-
-    let txUrlObj = {
+    const tx = {
       ...this.transaction,
-      ...additionalTxParams,
+      abi: this.abi
     };
-
-    return `ethereum:${txUrlObj.to}?${qs.stringify(txUrlObj)}`;
+    return `ethereum:${this.transaction.to}?${qs.stringify(this.transaction)}`;
   }
 }
