a8a288e0510ee83e2a385911b675541e6a5722c1
Zachary Belford <belfordz66@gmail.com>
2018-11-21 14:13:37 -0800

    detypescript

--- src/transactionUri.js
+++ src/transactionUri.js
@@ -1,36 +1,5 @@
 import { rlp } from 'ethereumjs-util';
 import * as qs from 'qs';
-import { BigNumber } from 'bignumber.js';
-
-type TAddress = string;
-
-interface ITransaction {
-  from?: TAddress;
-  to?: TAddress;
-  value?: BigNumber | string | number;
-  data?: string;
-  gas?: BigNumber | string | number;
-  gasPrice?: BigNumber | string | number;
-}
-
-export default class Transaction {
-  constructor(
-    public from?: TAddress,
-    public to?: TAddress,
-    public value?: BigNumber | string | number,
-    public data?: string,
-    public gas?: BigNumber | string | number,
-    public gasPrice?: BigNumber | string | number
-  ) {
-    this.from = from;
-    this.to = to;
-    this.value = value;
-    this.data = data;
-    this.gas = gas;
-    this.gasPrice = gasPrice;
-  }
-}
-
 
 class TransactionUri {
   constructor(transaction, abi) {
