7a96575b73100f0bc329f5371a1079c46b7fcd07
gagarin55 <gagarin55@protonmail.com>
2019-01-18 18:57:55 +0300

    solution: contracts - added Contract class

--- packages/contracts/typings/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'ethereumjs-abi' {
-    import { Buffer } from 'safe-buffer'
-  
-    export function soliditySHA3 (types: string[], values: any[]): Buffer
-    export function rawEncode (types: string[], values: any[]): Buffer
-    export function encodeSingle (type: string, value: any): Buffer
-    export function rawDecode<A> (types: string[], data: Buffer): A
-  }
