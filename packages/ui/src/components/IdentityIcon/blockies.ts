888013a32fbec19c513637dd3d38c1da2abadb6b
gagarin55 <gagarin55@protonmail.com>
2019-02-22 10:54:38 +0300

    solution: blockies translated to typescript

--- packages/ui/src/components/IdentityIcon/blockies.ts
+++ packages/ui/src/components/IdentityIcon/blockies.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
     // The random number is a js implementation of the Xorshift PRNG
-    var randseed = new Array(4) // Xorshift: [x, y, z, w] 32 bit values
+    var randseed = new Array(4); // Xorshift: [x, y, z, w] 32 bit values
 
     function seedrand (seed) {
         for (var i = 0; i < randseed.length; i++) {
@@ -14,46 +14,46 @@
 
     function rand () {
         // based on Java's String.hashCode(), expanded to 4 32bit values
-        var t = randseed[0] ^ (randseed[0] << 11)
+        var t = randseed[0] ^ (randseed[0] << 11);
 
-        randseed[0] = randseed[1]
-        randseed[1] = randseed[2]
-        randseed[2] = randseed[3]
-        randseed[3] = (randseed[3] ^ (randseed[3] >> 19) ^ t ^ (t >> 8))
+        randseed[0] = randseed[1];
+        randseed[1] = randseed[2];
+        randseed[2] = randseed[3];
+        randseed[3] = (randseed[3] ^ (randseed[3] >> 19) ^ t ^ (t >> 8));
 
         return (randseed[3] >>> 0) / ((1 << 31) >>> 0)
     }
 
     function createColor () {
         //saturation is the whole color spectrum
-        var h = Math.floor(rand() * 360)
+        var h = Math.floor(rand() * 360);
         //saturation goes from 40 to 100, it avoids greyish colors
-        var s = ((rand() * 60) + 40) + '%'
+        var s = ((rand() * 60) + 40) + '%';
         //lightness can be anything from 0 to 100, but probabilities are a bell curve around 50%
-        var l = ((rand() + rand() + rand() + rand()) * 25) + '%'
+        var l = ((rand() + rand() + rand() + rand()) * 25) + '%';
 
-        var color = 'hsl(' + h + ',' + s + ',' + l + ')'
+        var color = 'hsl(' + h + ',' + s + ',' + l + ')';
         return color
     }
 
     function createImageData (size) {
-        var width = size // Only support square icons for now
-        var height = size
+        var width = size; // Only support square icons for now
+        var height = size;
 
-        var dataWidth = Math.ceil(width / 2)
-        var mirrorWidth = width - dataWidth
+        var dataWidth = Math.ceil(width / 2);
+        var mirrorWidth = width - dataWidth;
 
-        var data = []
+        var data = [];
         for (var y = 0; y < height; y++) {
-            var row = []
+            var row = [];
             for (var x = 0; x < dataWidth; x++) {
                 // this makes foreground and background color to have a 43% (1/2.3) probability
                 // spot color has 13% chance
                 row[x] = Math.floor(rand() * 2.3)
             }
-            var r = row.slice(0, mirrorWidth)
-            r.reverse()
-            row = row.concat(r)
+            var r = row.slice(0, mirrorWidth);
+            r.reverse();
+            row = row.concat(r);
 
             for (var i = 0; i < row.length; i++) {
                 data.push(row[i])
@@ -64,43 +64,43 @@
     }
 
     function buildOpts (opts) {
-        var newOpts = {}
+        var newOpts: any = {};
 
-        newOpts.size = opts.size || 8
-        newOpts.scale = opts.scale || 4
-        newOpts.seed = opts.seed || Math.floor((Math.random() * Math.pow(10, 16))).toString(16)
+        newOpts.size = opts.size || 8;
+        newOpts.scale = opts.scale || 4;
+        newOpts.seed = opts.seed || Math.floor((Math.random() * Math.pow(10, 16))).toString(16);
 
-        seedrand(newOpts.seed)
+        seedrand(newOpts.seed);
 
-        newOpts.color = opts.color || createColor()
-        newOpts.bgcolor = opts.bgcolor || createColor()
-        newOpts.spotcolor = opts.spotcolor || createColor()
+        newOpts.color = opts.color || createColor();
+        newOpts.bgcolor = opts.bgcolor || createColor();
+        newOpts.spotcolor = opts.spotcolor || createColor();
 
         return newOpts
     }
 
     function renderIcon (opts, canvas) {
-        var opts = buildOpts(opts || {})
+        var opts: any = buildOpts(opts || {});
 
-        var imageData = createImageData(opts.size)
-        var width = Math.sqrt(imageData.length)
+        var imageData = createImageData(opts.size);
+        var width = Math.sqrt(imageData.length);
 
-        canvas.width = canvas.height = opts.size * opts.scale
+        canvas.width = canvas.height = opts.size * opts.scale;
 
-        var cc = canvas.getContext('2d')
-        cc.fillStyle = opts.bgcolor
-        cc.fillRect(0, 0, canvas.width, canvas.height)
-        cc.fillStyle = opts.color
+        var cc = canvas.getContext('2d');
+        cc.fillStyle = opts.bgcolor;
+        cc.fillRect(0, 0, canvas.width, canvas.height);
+        cc.fillStyle = opts.color;
 
         for (var i = 0; i < imageData.length; i++) {
 
             // if data is 0, leave the background
             if (imageData[i]) {
-                var row = Math.floor(i / width)
-                var col = i % width
+                var row = Math.floor(i / width);
+                var col = i % width;
 
                 // if data is 2, choose spot color, if 1 choose foreground
-                cc.fillStyle = (imageData[i] == 1) ? opts.color : opts.spotcolor
+                cc.fillStyle = (imageData[i] == 1) ? opts.color : opts.spotcolor;
 
                 cc.fillRect(col * opts.scale, row * opts.scale, opts.scale, opts.scale)
             }
@@ -109,10 +109,10 @@
     }
 
     function createIcon (opts) {
-        var opts = buildOpts(opts || {})
-        var canvas = document.createElement('canvas')
+        var opts = buildOpts(opts || {});
+        var canvas = document.createElement('canvas');
 
-        renderIcon(opts, canvas)
+        renderIcon(opts, canvas);
 
         return canvas
     }
@@ -120,13 +120,13 @@
     var api = {
         create: createIcon,
         render: renderIcon
-    }
+    };
 
     if (typeof module !== 'undefined') {
         module.exports = api
     }
     if (typeof window !== 'undefined') {
-        window.blockies = api
+      (<any>window).blockies = api
     }
 
 
