077aa59c54830b9616cefa08f5dcf1838dd99348
gagarin55 <gagarin55@protonmail.com>
2019-03-22 08:19:39 +0300

    Dependencies upgrade

--- packages/ui/stories/Page/index.tsx
+++ packages/ui/stories/Page/index.tsx
@@ -4,14 +4,13 @@ import Typography from '@material-ui/core/Typography';
 import { withKnobs, text } from '@storybook/addon-knobs/react';
 
 import Page from '../../src/components/Page';
-// import { Back } from '@emeraldplatform/ui-icons';
-import { ArrowBack } from '@material-ui/icons';
+import { Back } from '@emeraldplatform/ui-icons';
 
 storiesOf('Page', module)
   .addDecorator(withKnobs)
   .add('default', () => (
     <div>
-      <Page title={text('title', 'Title Here')} leftIcon={<ArrowBack />}>
+      <Page title={text('title', 'Title Here')} leftIcon={<Back />}>
         <Typography>Im a page content supsup</Typography>
       </Page>
     </div>
