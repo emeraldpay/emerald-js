3b2ae2f0a8bacc2d362d917b9f2b85b8fce95659
Yuri Gagarin <gagarin55@users.noreply.github.com>
2019-09-20 12:15:04 +0300

    solution: toBaseUnits and fromBaseUnits supports strings (#105)
    * solution: toBaseUnits and fromBaseUnits supports strings

--- packages/core/src/units.ts
+++ packages/core/src/units.ts
@@ -30,16 +30,18 @@ export class Units {
  * For example, amount 1 ether with 18 decimal places will be converted into 1*10^18 base units
  * (i.e. 1*10^18 wei)
  */
-export function toBaseUnits(amount: BigNumber, decimals: number): BigNumber {
+export function toBaseUnits(amount: BigNumber | string, decimals: number): BigNumber {
+  const v = (typeof amount === 'string') ? new BigNumber(amount) : amount;
   const unit = new BigNumber(10).pow(decimals);
-  return amount.times(unit);
+  return v.times(unit);
 }
 
 /**
  * Convert from smallest denomination (base units) to amount of token
  * For example, 1 wei will be converted to 0.000000000000000001 ether
  */
-export function fromBaseUnits(amount: BigNumber, decimals: number): BigNumber {
+export function fromBaseUnits(amount: BigNumber | string, decimals: number): BigNumber {
+  const v = (typeof amount === 'string') ? new BigNumber(amount) : amount;
   const unit = new BigNumber(10).pow(decimals);
-  return amount.div(unit);
+  return v.div(unit);
 }
