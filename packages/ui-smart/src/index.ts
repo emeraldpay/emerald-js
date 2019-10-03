543b0eaa1c002b9b7abb8a4abb4d61db1c0037bb
Igor Artamonov <igor@artamonov.ru>
2019-10-02 23:18:03 -0400

    problem: Vault is disabling http access; interfaces are moved to the wallet

--- packages/ui-smart/src/index.ts
+++ packages/ui-smart/src/index.ts
@@ -13,14 +13,11 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-export { default as AccountSelector } from './smart-components/AccountSelector';
 export { default as CurrentBlockNumber } from './smart-components/CurrentBlockNumber';
 export { default as EtcBalance } from './smart-components/Balance';
 export { default as NetworkSelector } from './smart-components/NetworkSelector';
 export { EmeraldProvider } from './providers/EmeraldProvider';
 export { default as EthJsonRpcProvider } from './providers/EthJsonRpcProvider';
 export { default as EthRpc } from './providers/EthRpc';
-export { default as VaultJsonRpcProvider } from './providers/VaultJsonRpcProvider';
-export { default as VaultRpc } from './providers/VaultRpc';
 export { default as Contract } from './providers/ContractProvider';
 export { default as TransactionUri } from './providers/TransactionUri';
